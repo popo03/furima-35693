c12ccda0dbb89e11f8d5e3fe83f54971202b4b89
popo03 <83389638+popo03@users.noreply.github.com>
2021-05-25 16:47:41 +0900

    エラー画面実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, inclusion: {in:('@')}
-  validates :encrypted_password, presence: true, format: {with: /\A[a-z0-9]+\z/i }, length: { minimum:6 }
+#  validates :email, presence: true, uniqueness: true
+#  validates :encrypted_password, presence: true, format: {with: /\A[a-z0-9]+\z/i }, length: { minimum:6 }
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "First name is invalid. Input full-width characters"}
   validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Family name is invalid. Input full-width characters"}
   validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "First name kana is invalid. Input full-width katakana characters"}
