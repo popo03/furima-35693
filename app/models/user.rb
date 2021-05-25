c7527a62df9c07a1360dd113314058fd635bad21
popo03 <83389638+popo03@users.noreply.github.com>
2021-05-25 10:29:24 +0900

    バリデーション導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, inclusion: {in:('@')}
+  validates :encrypted_password, presence: true, length: {minmum:6 }, format: {with: /\A[a-z0-9]+\z/i }
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/ }
+  validates :family_name, presence: true, format: {with: /\A[ァ-ヶー]+\z/ }
+  validates :birthday, presence: true
 end
