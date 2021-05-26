e6937cc4900aa05db5291c56f30f84d7b454188f
popo03 <83389638+popo03@users.noreply.github.com>
2021-05-26 21:31:56 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :product_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  #validates :category_id
+  #validates :status_id
+  #validates :burden_id
+  #validates :prefecture_id
+  #validates :date_id
 end
