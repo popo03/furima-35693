7b2aabe2efaceb31613f8cb5161ffd931006e271
popo03 <83389638+popo03@users.noreply.github.com>
2021-05-27 16:23:14 +0900

    createアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :description, :category_id, :status_id, :burden_id, :prefecture_id, :delivery_date_id).merge(user_id: current_user.id)
+  end
+
 end
