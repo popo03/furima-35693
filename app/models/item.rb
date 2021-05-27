61284a13b8bcbcd35cf278bf272aec3d2bf7037b
popo03 <83389638+popo03@users.noreply.github.com>
2021-05-27 10:39:48 +0900

    各modelの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :date
+
   belongs_to :user
   has_one_attached :image
 
@@ -6,9 +13,9 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
   validates :image, presence: true
-  #validates :category_id
-  #validates :status_id
-  #validates :burden_id
-  #validates :prefecture_id
-  #validates :date_id
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :burden_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :date_id, numericality: { other_than: 1 }
 end
